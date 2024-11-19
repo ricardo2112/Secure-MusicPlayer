0285c43cab51a7eb6f6d092ecdd79d1764b0a2f8
Ketif2 <kevin.revelo01@epn.edu.ec>
2024-11-19 14:29:09 -0500

    feat(frontend): initialize React + Vite setup with TailwindCSS and ESLint configuration

---
+++ packages/frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-500 text-white">
+      <h1 className="text-4xl font-bold">¡TailwindCSS está funcionando!</h1>
+    </div>
+  )
+}
+
+export default App
