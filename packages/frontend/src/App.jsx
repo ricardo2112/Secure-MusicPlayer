190180b734a97e7d2ab52e976ecac11871f8ed01
Ketif2 <kevin.revelo01@epn.edu.ec>
2024-11-21 15:06:39 -0500

    feat(frontend): implement routing and add audio player component with navigation

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -1,10 +1,22 @@
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import MusicPlayer from './pages/MusicPlayer';
+import NotFound from './pages/NotFound';
+
 function App() {
-
-
   return (
-    <div className="h-screen flex items-center justify-center bg-blue-500 text-white">
-      <h1 className="text-4xl font-bold">¡TailwindCSS está funcionando!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/musicplayer" element={<MusicPlayer />} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
