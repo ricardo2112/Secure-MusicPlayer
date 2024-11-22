66c3cb114c0139d7314f6407a453807be25f0d78
Ketif2 <kevin.revelo01@epn.edu.ec>
2024-11-22 08:04:56 -0500

    feat(frontend): add new components for Music List, Favorites, Playlists, Community, and Sidebar; update AudioPlayer and MainContent; enhance styling with new fonts and colors

---
+++ packages/frontend/src/components/PlayList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Playlists = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-bold">Tus playlists</h1>
+      {/* Contenido del componente */}
+    </div>
+  );
+};
+
+export default Playlists;
---
+++ packages/frontend/src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Sidebar = ({ onSectionChange }) => {
+    const menuItems = [
+        { id: 1, label: 'Music List', component: 'MusicList' },
+        { id: 2, label: 'Favoritas', component: 'Favoritas' },
+        { id: 3, label: 'Playlists', component: 'Playlists' },
+        { id: 4, label: 'Comunidad', component: 'Comunidad' },
+    ];
+  return (
+    <div className='w-full bg-secondary text-white p-4 border-r border-contrast flex flex-col h-full'>
+        <nav className="flex flex-col gap-2">
+            {menuItems.map((item) => (
+                <button
+                    key={item.id}
+                    onClick={() => onSectionChange(item.component)}
+                    className='w-full text-left py-2 px-4 hover:bg-primary rounded-lg mb-1'
+                >{item.label}
+                </button>
+            ))}
+        </nav>
+    </div>
+  )
+}
+
+export default Sidebar
