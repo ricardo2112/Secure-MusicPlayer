df1485910b221aa9db3fcaf2b65d6e2da5889721
Ferengi2002 <juan.rengifo@epn.edu.ec>
2024-11-24 00:04:17 -0500

    add. Playlist

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -1,3 +1,4 @@
+// App.jsx
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
@@ -5,28 +6,31 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import MusicPlayer from "./pages/MusicPlayer";
 import NotFound from "./pages/NotFound";
-import { ChatProvider } from "./context/ChatContext"; // Importa el ChatProvider
+import { ChatProvider } from "./context/ChatContext";
+import { PlaylistsProvider } from "./context/PlaylistsContext"; // Importar el proveedor
 
 function App() {
   return (
     <ChatProvider>
-      <BrowserRouter>
-        <div className="h-screen flex flex-col">
-          <div className="bg-blue-600 text-white p-4">
-            <h1 className="text-center text-xl font-bold">Music Application</h1>
-          </div>
+      <PlaylistsProvider>
+        <BrowserRouter>
+          <div className="h-screen flex flex-col">
+            <div className="bg-blue-600 text-white p-4">
+              <h1 className="text-center text-xl font-bold">Music Application</h1>
+            </div>
 
-          <div className="flex-1">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/musicplayer" element={<MusicPlayer />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
+            <div className="flex-1">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/musicplayer" element={<MusicPlayer />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </div>
           </div>
-        </div>
-      </BrowserRouter>
+        </BrowserRouter>
+      </PlaylistsProvider>
     </ChatProvider>
   );
 }
