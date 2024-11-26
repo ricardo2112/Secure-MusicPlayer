052b39d373d1aa35956eb7911cd2e99dcc1da93b
Ketif2 <kevin.revelo01@epn.edu.ec>
2024-11-26 07:44:27 -0500

    fix(backend/musicController): improve error handling and restructure try-catch blocks

--- packages/frontend/src/services/data.js
+++ packages/frontend/src/services/data.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 
 const BACKEND_API_URL = 'http://localhost:3000';
 
+// Recupera lista de canciones populares
 export const fetchTrendingTracksData = async () => {
     try {
-        const response = await axios.get(`${BACKEND_API_URL}/trending`);
+        const response = await axios.get(`${BACKEND_API_URL}/api/trending`);
         return response.data;
     } catch (error) {
         console.error('Error fetching trending tracks:', error);
@@ -12,24 +13,35 @@ export const fetchTrendingTracksData = async () => {
     }
 };
 
+// Recupera lista de canciones relajantes de generos aleatorios
 export const fetchRelaxingTracksData = async () => {
-    const genres = ['chill', 'ambient', 'acoustic', 'classical', 'jazz', 'lo-fi', 'soul'];
-    const randomGenre = genres[Math.floor(Math.random() * genres.length)];
     try {
-        const response = await axios.get(`https://discoveryprovider.audius.co/v1/tracks/trending?genre=${randomGenre}`);
-        return response;
+        const response = await axios.get(`${BACKEND_API_URL}/relax`);
+        return response.data;
     } catch (error) {
         console.error('Error fetching relaxing tracks:', error);
         throw error;
     }
 };
- 
+
+// Obtener detalles de una canción por ID
 export const fetchTrackDetails = async (trackId) => {
     try {
-        const response = await axios.get(`https://discoveryprovider.audius.co/v1/tracks/${trackId}`);
-        return response;
+        const response = await axios.get(`${BACKEND_API_URL}/songs/${trackId}`);
+        return response.data;
     } catch (error) {
         console.error('Error fetching track details:', error);
+        throw error;
+    }
+};
+
+// Obtener la URL de streaming de una canción
+export const fetchSongStream = async (trackId) => {
+    try {
+        const response = await axios.get(`${BACKEND_API_URL}/songs/${trackId}/play`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching song stream:', error);
         throw error;
     }
 };
